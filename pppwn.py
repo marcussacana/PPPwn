d8e2decedf409960beeb69dcbbbbf8b0e149c37f
Marcus André <ddtank.marcus@gmail.com>
2024-05-04 00:55:50 -0300

    Update

--- pppwn.py
+++ pppwn.py
@@ -878,7 +878,7 @@ class Exploit():
 def main():
     parser = ArgumentParser('pppwn.py')
     parser.add_argument('--interface', required=True)
-    parser.add_argument('--fw', choices=['900', '1100'], default='1100')
+    parser.add_argument('--fw', choices=['672', '900', '1100'], default='1100')
     parser.add_argument('--stage1', default='stage1/stage1.bin')
     parser.add_argument('--stage2', default='stage2/stage2.bin')
     args = parser.parse_args()
@@ -892,7 +892,9 @@ def main():
     with open(args.stage2, mode='rb') as f:
         stage2 = f.read()
 
-    if args.fw == '900':
+    if args.fw == '672':
+        offs = OffsetsFirmware_672()
+    elif args.fw == '900':
         offs = OffsetsFirmware_900()
     elif args.fw == '1100':
         offs = OffsetsFirmware_1100()
